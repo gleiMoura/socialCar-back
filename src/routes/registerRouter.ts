9fef92f6d547f80cb9754da3741f4be6bb40912e
gleiMoura <gle.bar.mou@gmail.com>
2024-11-18 16:56:29 -0300

    ✨ feat: create a register router

---
+++ src/routes/registerRouter.ts
@@ -0,0 +1,9 @@
+import { doRegister } from "controllers/registerController";
+import { Router } from "express";
+import schemaValidator from "middlewares/schemaValidator";
+import registerSchema from "schemas/registerSchema";
+
+const registerRouter = Router();
+registerRouter.post('/register', schemaValidator(registerSchema), doRegister)
+
+export default registerRouter;
