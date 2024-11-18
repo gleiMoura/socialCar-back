00de30dfdcba9357f883d57e8856cf2165e59ebe
gleiMoura <gle.bar.mou@gmail.com>
2024-11-18 16:56:13 -0300

    ✨ feat: create a login router

---
+++ src/routes/loginRouter.ts
@@ -0,0 +1,9 @@
+import { doLogin } from "controllers/loginController";
+import { Router } from "express";
+import schemaValidator from "middlewares/schemaValidator";
+import loginSchema from "schemas/loginSchema";
+
+const loginRouter = Router();
+loginRouter.post("/login", schemaValidator(loginSchema), doLogin);
+
+export default loginRouter;
