bf8dfae1d90beff7dcd5d0c7129f27e9c6256d3b
gleiMoura <gle.bar.mou@gmail.com>
2024-11-18 16:55:58 -0300

    ✨ feat: create an express router file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import Router from "express";
+import loginRouter from "./loginRouter";
+import registerRouter from "./registerRouter";
 
 const router = Router();
+router.use(loginRouter)
+router.use(registerRouter)
 
 export default router;
