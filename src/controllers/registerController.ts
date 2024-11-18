5a1916941ac5b83f847825019cccdf04691396da
gleiMoura <gle.bar.mou@gmail.com>
2024-11-18 16:54:35 -0300

    ✨ feat: create register controller

---
+++ src/controllers/registerController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+import { logUser } from "services/registerService";
+
+export const doRegister = async (req: Request, res: Response) => {
+    const credentials = req.body;
+
+    await logUser(credentials);
+
+    res.sendStatus(201);
+};
