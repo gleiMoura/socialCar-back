4b63d34d7abbb2da0fcaf074e865a50c7f308e83
gleiMoura <gle.bar.mou@gmail.com>
2024-12-03 13:10:27 -0300

    ✨ feat: create a router to update register with profile photo

--- src/routes/registerRouter.ts
+++ src/routes/registerRouter.ts
@@ -1,9 +1,14 @@
-import { doRegister } from "../controllers/registerController";
+import { doRegister, insertProfileImage } from "../controllers/registerController";
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator";
 import registerSchema from "../schemas/registerSchema";
+import multer from "multer"
+
+const upload = multer({ dest: 'uploads/' });
 
 const registerRouter = Router();
+
 registerRouter.post('/register', schemaValidator(registerSchema), doRegister)
+registerRouter.put('/register/profile', upload.single('file'), insertProfileImage)
 
 export default registerRouter;
