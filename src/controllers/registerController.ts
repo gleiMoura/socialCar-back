eac1f24a2a0fd28859b41c254be3e2278b5e5355
gleiMoura <gle.bar.mou@gmail.com>
2024-12-03 13:08:06 -0300

    ✨ feat: create a function Controller to add profile image

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,6 +1,7 @@
+import { generateProfileLink } from "repository/filesRepository";
 import { Request, Response } from "express";
 
-import { logUser } from "../services/registerService";
+import { logUser, logUserWithProfileLink } from "../services/registerService";
 
 export const doRegister = async (req: Request, res: Response) => {
     const credentials = req.body;
@@ -8,4 +9,18 @@ export const doRegister = async (req: Request, res: Response) => {
     await logUser(credentials);
 
     res.sendStatus(201);
+};
+
+
+export const insertProfileImage = async (req: Request, res: Response) => {
+    const file = req.file;
+    const token = req.body.token;
+
+    console.log("token", token)
+
+    const profileLink = await generateProfileLink(file);
+
+    const profile = await logUserWithProfileLink(token, profileLink);
+
+    res.status(201).send(profile);
 };
