222fbcbe7fe28d7bfeadc5bd8fde146d81375e4c
gleiMoura <gle.bar.mou@gmail.com>
2024-12-03 13:08:52 -0300

    ✨ feat: create a file repository

---
+++ src/repository/filesRepository.ts
@@ -0,0 +1,22 @@
+import { bucket, bucketName } from "config/cloud";
+
+export const generateProfileLink = async (file) => {
+    try {
+        const filePath = file.path;
+        const fileName = file.originalname;
+        const destination = bucket.file(fileName);
+
+        // Enviando o arquivo para o Google Cloud Storage
+        await bucket.upload(filePath, {
+            destination: fileName,
+        });
+
+        // Tornando o arquivo público
+        await destination.makePublic();
+
+        const publicUrl = `https://storage.googleapis.com/${bucketName}/${fileName}`;
+        return publicUrl;
+    } catch (error) {
+        console.error('Erro no upload:', error);
+    }
+};
