961c4360a9af6a26fc3b8d91496308688a27b8ea
gleiMoura <gle.bar.mou@gmail.com>
2024-12-06 19:33:13 -0300

    🐛 fix: delete and get file from front

--- src/repository/filesRepository.ts
+++ src/repository/filesRepository.ts
@@ -1,22 +1,48 @@
 import { bucket, bucketName } from "../config/cloud.js";
 
+// Set para controlar arquivos que estão em processo de upload
+const uploadInProgress = new Set();
+
 export const generateProfileLink = async (file) => {
     try {
+        if (!file || !file.path || !file.originalname) {
+            throw new Error("File is not valid or missing required properties");
+        }
+
         const filePath = file.path;
         const fileName = file.originalname;
+
+        // Verificar se o arquivo já está em processo de upload
+        if (uploadInProgress.has(fileName)) {
+            throw new Error('Upload já está em progresso para esse arquivo.');
+        }
+
+        // Adiciona o arquivo ao conjunto de uploads em progresso
+        uploadInProgress.add(fileName);
+
         const destination = bucket.file(fileName);
 
-        // Enviando o arquivo para o Google Cloud Storage
+        // Realiza o upload
         await bucket.upload(filePath, {
             destination: fileName,
         });
 
-        // Tornando o arquivo público
+        // Torna o arquivo público
         await destination.makePublic();
 
+        // Gera o link público
         const publicUrl = `https://storage.googleapis.com/${bucketName}/${fileName}`;
+
+        // Remove o arquivo do conjunto após o upload
+        uploadInProgress.delete(fileName);
+
         return publicUrl;
     } catch (error) {
         console.error('Erro no upload:', error);
+        // Remove o arquivo do conjunto em caso de erro também
+        if (file && file.originalname) {
+            uploadInProgress.delete(file.originalname);
+        }
+        throw new Error('Falha ao gerar link do perfil: ' + error.message);
     }
 };
