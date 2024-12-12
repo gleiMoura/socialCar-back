946393db6d7e9d171aaceb08d4f313d30bb3a53f
gleiMoura <gle.bar.mou@gmail.com>
2024-12-11 21:53:52 -0300

    🔧 add credentials without path

--- src/config/cloud.ts
+++ src/config/cloud.ts
@@ -1,8 +1,27 @@
 import { Storage } from '@google-cloud/storage';
+import { config } from 'dotenv';
+
+config();
+
+const credentials = {
+    type: process.env.GOOGLE_CREDENTIALS_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+};
+
+console.log(credentials)
 
 const storage = new Storage({
-    keyFilename: '/home/ubuntuglei/socialCar-back/src/credentials.json', // Caminho para o arquivo de credenciais
-    projectId: 'socialcar-442819', // Substitua pelo ID do seu projeto
+    projectId: 'socialcar-442819',
+    credentials: credentials
 });
 
 
