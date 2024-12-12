c022d7f31f74013ea867d30de0ef594638545832
gleiMoura <gle.bar.mou@gmail.com>
2024-12-11 21:54:12 -0300

    ⚰️ remove dead code

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -18,10 +18,8 @@ export const insertProfileImage = async (req: Request, res: Response) => {
     const token = authHeader.split(' ')[1];
 
     const profileLink = await generateProfileLink(file);
-    console.log("profileLink:", profileLink)
 
     const profile = await logUserWithProfileLink(token, profileLink);
-    console.log("profile", profile)
 
     res.status(201).send(profile);
 };
