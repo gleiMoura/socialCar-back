6e7a28b3d7bb253a8bdb1a7e30a5f665df45b02b
gleiMoura <gle.bar.mou@gmail.com>
2024-12-12 16:20:57 -0300

    ♻️ refactor: put dot

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import Router from "express";
 import loginRouter from "./loginRouter.js";
 import registerRouter from "./registerRouter.js";
+import quizzRouter from "./quizzRouter.js";
 
 const router = Router();
-router.use(loginRouter)
-router.use(registerRouter)
+router.use(loginRouter);
+router.use(registerRouter);
+router.use(quizzRouter);
 
 export default router;
