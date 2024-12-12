f41331c643c7cc5daa9f1f83f8627dbb4102ff12
gleiMoura <gle.bar.mou@gmail.com>
2024-12-12 16:18:58 -0300

    ♻️ refactor: change code to add port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 app.listen(process.env.PORT || "5000", () => {
-    console.log("Server is running!");
+    console.log(`Server is running on port ${process.env.PORT || 5000}!`);
 });
 
 db //show that database is working in node!
