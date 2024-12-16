04ebccf08aa404eea79b3284a5ae9b037d85c79f
gleiMoura <gle.bar.mou@gmail.com>
2024-12-16 20:17:11 -0300

    ✨ feat: create a router to delete a quizz

--- src/routes/quizzRouter.ts
+++ src/routes/quizzRouter.ts
@@ -1,4 +1,4 @@
-import { doQuizz, sendAllQuizzes, sendUserQuizzes } from "controllers/quizzController.js";
+import { deleteQuizz, doQuizz, sendAllQuizzes, sendUserQuizzes } from "controllers/quizzController.js";
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
 import quizzSchema from "schemas/quizSchema.js";
@@ -7,7 +7,7 @@ const quizzRouter = Router();
 quizzRouter.post("/quizz", schemaValidator(quizzSchema), doQuizz);
 quizzRouter.get("/quizz", sendAllQuizzes);
 quizzRouter.get("/quizz/user", sendUserQuizzes);
-//quizzRouter.delete("/quizz/:id", deleteQuizz);
+quizzRouter.delete("/quizz/:id", deleteQuizz);
 
 
 export default quizzRouter;
