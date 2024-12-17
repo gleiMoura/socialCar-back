17d935a5c732e2810537d71ae39db2f0fa437c65
gleiMoura <gle.bar.mou@gmail.com>
2024-12-16 22:42:18 -0300

    ♻️ refactor: change module names

--- src/routes/quizzRouter.ts
+++ src/routes/quizzRouter.ts
@@ -1,7 +1,7 @@
-import { deleteQuizz, doQuizz, sendAllQuizzes, sendUserQuizzes } from "controllers/quizzController.js";
+import { deleteQuizz, doQuizz, sendAllQuizzes, sendUserQuizzes } from "../controllers/quizzController.js";
 import { Router } from "express";
 import schemaValidator from "../middlewares/schemaValidator.js";
-import quizzSchema from "schemas/quizSchema.js";
+import quizzSchema from "../schemas/quizSchema.js";
 
 const quizzRouter = Router();
 quizzRouter.post("/quizz", schemaValidator(quizzSchema), doQuizz);
