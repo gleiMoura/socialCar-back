4a8dbff0c6d5de8a0d5b38280941f8e1a2f58cf4
gleiMoura <gle.bar.mou@gmail.com>
2024-12-16 22:54:26 -0300

    ✨ feat: create a repository function to get all posts and user post

--- src/repository/postRepository.ts
+++ src/repository/postRepository.ts
@@ -11,3 +11,25 @@ export const savePostInDb = async (post: PostType) => {
         throw error
     }
 };
+
+export const getPostsFromDb = async () => {
+    try {
+        const db = await database;
+        const result = await db.collection('post').find().toArray();
+        return result;
+    } catch (error) {
+        console.log("Error trying to save quizz", error);
+        throw error
+    }
+};
+
+export const getUserPostsFromDb = async (userId: string) => {
+    try {
+        const db = await database;
+        const result = await db.collection('post').find({ userId }).toArray();
+        return result;
+    } catch (error) {
+        console.log("Error trying to save quizz", error);
+        throw error
+    }
+};
