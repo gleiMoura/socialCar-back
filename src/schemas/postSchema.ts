dc51361d11a10c431a48805ba99c306c5c457bdb
gleiMoura <gle.bar.mou@gmail.com>
2024-12-16 22:42:58 -0300

    ✨ feat: create post schema

---
+++ src/schemas/postSchema.ts
@@ -0,0 +1,8 @@
+import { registerType } from "interfaces/index.js";
+import joi, { ObjectSchema } from "joi";
+
+const postSchema: ObjectSchema<registerType> = joi.object({
+    caption: joi.string().required(),
+});
+
+export default postSchema;
