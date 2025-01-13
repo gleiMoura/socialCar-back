d16beeb97835e6eeb75e4a9a2def3f7fbf2a2055
gleiMoura <gle.bar.mou@gmail.com>
2025-01-13 18:34:43 -0300

    ✨ feat: create a router to get a quizz by id

--- src/routes/quizzRouter.ts
+++ src/routes/quizzRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteQuizz, doQuizz, sendAllQuizzes, sendUserQuizzes } from "../controllers/quizzController.js";
+import { deleteQuizz, doQuizz, getQuizzById, sendAllQuizzes, sendUserQuizzes } from "../controllers/quizzController.js";
 import schemaValidator from "../middlewares/schemaValidator.js";
 import quizzSchema from "../schemas/quizSchema.js";
 
@@ -8,6 +8,7 @@ quizzRouter.post("/quizz", schemaValidator(quizzSchema), doQuizz);
 quizzRouter.get("/quizz", sendAllQuizzes);
 quizzRouter.get("/quizz/user", sendUserQuizzes);
 quizzRouter.delete("/quizz/:id", deleteQuizz);
+quizzRouter.get("/quizz/:id", getQuizzById);
 
 
 export default quizzRouter;
