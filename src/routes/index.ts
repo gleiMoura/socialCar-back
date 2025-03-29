f4791a5677d717ee58732a70a718102d538324a7
gleiMoura <gle.bar.mou@gmail.com>
2025-03-29 19:03:12 -0300

    🐛 fix: delete code that doesn't exist anymore

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,11 @@ import loginRouter from "./loginRouter.js";
 import registerRouter from "./registerRouter.js";
 import quizzRouter from "./quizzRouter.js";
 import postsRouter from "./postsRouter.js";
-import imageRouter from "./createImageRoute.js";
-
 
 const router = Router();
 router.use(loginRouter);
 router.use(registerRouter);
 router.use(quizzRouter);
 router.use(postsRouter);
-router.use(imageRouter)
 
 export default router;
