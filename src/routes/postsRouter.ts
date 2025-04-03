ee36cfa771c18b859c97c209247ad3363b842edf
gleiMoura <gle.bar.mou@gmail.com>
2025-04-03 16:39:18 -0300

    ✨ feat: create a router function to delete a post

--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
-import { savePost, sendAllPosts, sendUserPosts } from "../controllers/postController.js";
+import { deletePost, savePost, sendAllPosts, sendUserPosts } from "../controllers/postController.js";
 
 const upload = multer({ dest: 'uploads/' });
 
@@ -8,5 +8,6 @@ const postsRouter = Router();
 postsRouter.post("/post", upload.single('file'), savePost);
 postsRouter.get("/post", sendAllPosts);
 postsRouter.get("/post/user", sendUserPosts);
+postsRouter.delete("/post/:id", deletePost)
 
 export default postsRouter;
